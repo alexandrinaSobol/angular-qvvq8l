0a6242e5e4f57aa58b83ea4b2bcee82a1a283141
alexandrinaSobol <alexandrina.sobol@isa.utm.md>
2021-03-03 18:07:35 +0200

    5 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProductDetailsComponent } from "./product-details/product-details.compo
 import { CartService } from "./cart.service";
 import { CartComponent } from "./cart/cart.component";
 import { ShippingComponent } from "./shipping/shipping.component";
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   imports: [
@@ -23,7 +24,8 @@ import { ShippingComponent } from "./shipping/shipping.component";
       { path: "", component: ProductAlertsComponent },
       { path: "products/:productId", component: ProductDetailsComponent },
       { path: "cart", component: CartComponent },
-      { path: "shipping", component: ShippingComponent }
+      { path: "shipping", component: ShippingComponent },
+      {path: "test", component: TestComponent},
     ])
   ],
   declarations: [
@@ -33,7 +35,8 @@ import { ShippingComponent } from "./shipping/shipping.component";
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
-    ShippingComponent
+    ShippingComponent,
+    TestComponent
   ],
   bootstrap: [AppComponent],
   providers: [CartService]
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,10 +11,17 @@ export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert("The product has been shared!");
+  window.alert("The product has been shared!");
   }
 
-  onNotify() {
+  onNotify(){
     window.alert("You will be notified when the product goes on sale");
   }
-}
+
+
+console(value: number){
+
+ const product = products.find(product => product.id === value);
+
+  console.log("ati adaugat produsul in cos "+ product.name)
+}}
